9767714d8603c99d28fac408c9e19d1abff42705
icouldonlygetthisname <101072895+icouldonlygetthisname@users.noreply.github.com>
2022-08-15 11:10:07 +0100

    Update deleteoldusers.ps1
    spelling mistakes corrected

--- deleteoldusers.ps1
+++ deleteoldusers.ps1
@@ -1,7 +1,7 @@
-## Varibles to set ##
+## Variables to set ##
 
 # Path to output the get-users list
-$path4 = 'C:\Users\someuser\Documents\deleteusers.txt'
+$path4 = 'C:\Users\administrator.DOMAIN\Documents\deleteusers.txt'
 
 # Date for last modified on Objects in OU to delete
 $date = (Get-Date).Adddays(-(7))
@@ -9,7 +9,7 @@ $date = (Get-Date).Adddays(-(7))
 # OU to delete users from
 $OU = "OU=Disabled Users,DC=domain,DC=local"
 
-## End Varibles ##
+## End Variables ##
 
 # Get list of users in OU and pipe to a file based of modified date
 Get-ADuser -Filter 'Modified -le $date' -SearchBase "$OU" -Properties SamAccountName | Select-Object -ExpandProperty SamAccountName | Out-file -FilePath $path4 -Force
@@ -23,3 +23,4 @@ Foreach($user in $users)
       Remove-ADObject -identity "$userGUID" -Confirm:$false 
       
     }
+
